969514549e02e58f45d8076149dd4d92788098df
kristz11 <kkristi.ri@gmail.com>
2026-06-15 17:17:50 +0500

    Исправлен синтаксис DATABASE_URL

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/shiftbrew")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/railway")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
